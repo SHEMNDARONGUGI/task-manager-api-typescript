7a0ac48f078744945c67ca61c20a2460331474fd
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-08-01 23:52:56 +0300

    working with GET request

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import e from "express";
+import e, { response } from "express";
 import express from "express";
+import { request } from "http";
 
 // express => creates an express application instance
 const app = express();
@@ -16,6 +17,29 @@ app.use(express.json());
 // res(response) => allow us to send something back
 app.get("/", (req, res) => {
   res.json({ message: "Task Manager API is running" });
+});
+
+app.get("/tasks", (req, res) => {
+  const tasks = [
+    {
+      id: 1,
+      title: "Learning express with typescript",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Building Task Manager API",
+      completed: true,
+    },
+  ];
+
+  // Why return an object instead of just the array?
+  //This is a common API pattern because later we can easily add more information (like pagination or messages) without changing the overall response structure.
+  res.status(200).json({
+    success: true,
+    count: tasks.length,
+    data: tasks,
+  });
 });
 
 export default app;

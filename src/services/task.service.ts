aa1d883da6d70df774470ba37a56e251591d91ad
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-08-06 18:58:32 +0300

    Refactored task.service.ts

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -3,3 +3,22 @@ import { tasks } from "../data/tasks.js";
 export const getAllTasksService = () => {
   return tasks;
 };
+
+export const getTaskByIdService = (id: number) => {
+  return tasks.find((task) => task.id === id);
+};
+
+export const createTaskService = (title: string, completed: boolean) => {
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    completed,
+  };
+
+  tasks.push(newTask);
+
+  return newTask;
+};
+
+export const updateTaskService = () => {};
+export const deleteTaskService = () => {};
